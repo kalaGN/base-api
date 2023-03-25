e04ad7481048cfcff5c61414a40af9dd7ab81e4e
fei.wang <fei.wang@yulore.com>
2023-03-25 10:24:09 +0800

    引入gin  测试样例

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+func main() {
+	r := gin.Default()
+	r.POST("/index/index", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"msg":    "pong",
+			"status": 2006,
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
